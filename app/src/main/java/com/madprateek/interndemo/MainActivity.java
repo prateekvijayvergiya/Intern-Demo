b6c7dc6250a936701cc4b467c1d3dd3adceae5e9
prateekvijayvergiya <prateek.vijayudr27@gmail.com>
2019-01-08 00:21:06 +0530

    Frame layout added

--- app/src/main/java/com/madprateek/interndemo/MainActivity.java
+++ app/src/main/java/com/madprateek/interndemo/MainActivity.java
@@ -1,13 +1,16 @@
 package com.madprateek.interndemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.HorizontalScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private HorizontalScrollView mHorizontalView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
